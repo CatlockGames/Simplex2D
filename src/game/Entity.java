c35d14f234ec4bc4791a2c8bf6e5e1568af45e01
Ryan Shappell <hoovcat99@gmail.com>
2015-05-20 22:16:40 +0900

    Add multiple hitbox capability

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -4,6 +4,7 @@
 package game;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 import math.Vector2d;
 
@@ -15,16 +16,16 @@ import math.Vector2d;
 public class Entity {
 	private Vector2d position;
 	private Vector2d velocity;
-	private Hitbox hitbox;
+	private ArrayList<Hitbox> hitboxes;
 
 	/**
 	 * 
 	 * @param position
 	 * @param hitbox
 	 */
-	public Entity(Vector2d position, Hitbox hitbox) {
+	public Entity(Vector2d position, ArrayList<Hitbox> hitboxes) {
 		this.position = position;
-		this.hitbox = hitbox;
+		this.hitboxes = hitboxes;
 	}
 	
 	/**
@@ -53,39 +54,45 @@ public class Entity {
 	 */
 	public boolean collides(Entity e){
 		boolean collides = false;
-		
-		int edgeCount = hitbox.vertices.length;
-		int eEdgeCount = e.hitbox.vertices.length;
+		int edgeCount;
+		int eEdgeCount;
 		Vector2d edge;
 		Vector2d axis;
 		double[] interval;
 		double[] eInterval;
 		
-		//If aabb start sat method
-		if(aabb(e)){
-			collides = true;
-			for(int i = 0; i < edgeCount + eEdgeCount; i++){
-				if (i < edgeCount) {
-		            edge = hitbox.edges[i];
-		        } else{
-		            edge = e.hitbox.edges[i - edgeCount];
-		        }
-				//Find the normalized axis perpendicular to the current edge.
-				axis = new Vector2d(-edge.y, edge.x);
-				axis = axis.normalize(axis);
+		for(int hitbox = 0; hitbox < hitboxes.size(); hitbox++){
+			for(int ehitbox = 0; ehitbox < e.hitboxes.size(); ehitbox++){
+				edgeCount = hitboxes.get(hitbox).vertices.length;
+				eEdgeCount = e.hitboxes.get(ehitbox).vertices.length;
 				
-				//Project the hitboxes onto the current axis.
-				interval = projectOnAxis(axis);
-				eInterval = e.projectOnAxis(axis);
-				
-				//Check if the projected intervals do not intersect.
-				//No intersection means no collision
-				if(interval[1] < eInterval[0] || eInterval[1] < interval[0]){
-					collides = false;
-					break;
+				//If aabb start sat method
+				if(aabb(e, hitbox, ehitbox)){
+					collides = true;
+					for(int i = 0; i < edgeCount + eEdgeCount; i++){
+						if (i < edgeCount) {
+				            edge = hitboxes.get(hitbox).edges[i];
+				        } else{
+				            edge = e.hitboxes.get(ehitbox).edges[i - edgeCount];
+				        }
+						//Find the normalized axis perpendicular to the current edge.
+						axis = new Vector2d(-edge.y, edge.x);
+						axis = axis.normalize(axis);
+						
+						//Project the hitboxes onto the current axis.
+						interval = projectOnAxis(axis, hitbox);
+						eInterval = e.projectOnAxis(axis, ehitbox);
+						
+						//Check if the projected intervals do not intersect.
+						//No intersection means no collision
+						if(interval[1] < eInterval[0] || eInterval[1] < interval[0]){
+							collides = false;
+							break;
+						}
+						
+						//Check if the hitboxes will intersect
+					}
 				}
-				
-				//Check if the hitboxes will intersect
 			}
 		}
 		
@@ -97,13 +104,13 @@ public class Entity {
 	 * @param e
 	 * @return
 	 */
-	private boolean aabb(Entity e){
+	private boolean aabb(Entity e, int hitbox, int ehitbox){
 		boolean aabb = false;
 		
-		double[] xInterval = projectOnAxis(new Vector2d(1, 0));
-		double[] exInterval = e.projectOnAxis(new Vector2d(1, 0));
-		double[] yInterval = projectOnAxis(new Vector2d(0, 1));
-		double[] eyInterval = e.projectOnAxis(new Vector2d(0, 1));
+		double[] xInterval = projectOnAxis(new Vector2d(1, 0), hitbox);
+		double[] exInterval = e.projectOnAxis(new Vector2d(1, 0), ehitbox);
+		double[] yInterval = projectOnAxis(new Vector2d(0, 1), hitbox);
+		double[] eyInterval = e.projectOnAxis(new Vector2d(0, 1), ehitbox);
 		
 		if((xInterval[1] >= exInterval[0] && exInterval[1] >= xInterval[0]) || (yInterval[1] >= eyInterval[0] && eyInterval[1] >= yInterval[0])){
 			aabb = true;
@@ -117,12 +124,12 @@ public class Entity {
 	 * @param axis
 	 * @return
 	 */
-	public double[] projectOnAxis(Vector2d axis){
-		double dotProduct = hitbox.getVertices(position)[0].dotProduct(axis);
+	public double[] projectOnAxis(Vector2d axis, int hitbox){
+		double dotProduct = hitboxes.get(hitbox).getVertices(position)[0].dotProduct(axis);
 		double[] interval = {dotProduct, dotProduct};
 		
-		for(int i = 1; i < hitbox.getVertices(position).length; i++){
-			dotProduct = hitbox.getVertices(position)[i].dotProduct(axis);
+		for(int i = 1; i < hitboxes.get(hitbox).getVertices(position).length; i++){
+			dotProduct = hitboxes.get(hitbox).getVertices(position)[i].dotProduct(axis);
 			if(dotProduct < interval[0]){
 				interval[0] = dotProduct;
 			} else if(dotProduct > interval[1]){
@@ -136,20 +143,38 @@ public class Entity {
 	public static void main(String[] args){
 		Vector2d[] hitboxVertices = {
 				new Vector2d(1, 0),
-				new Vector2d(0, 2),
-				new Vector2d(3, 2),
+				new Vector2d(1, 2),
+				new Vector2d(0, 3)
 		};
 		Vector2d[] hitboxVertices2 = {
-				new Vector2d(0, 1),
 				new Vector2d(1, 0),
-				new Vector2d(2, 1),
+				new Vector2d(2, 3),
+				new Vector2d(1, 2)
+		};
+		Vector2d[] hitboxVertices3 = {
+				new Vector2d(0, 0),
+				new Vector2d(1, 1),
+				new Vector2d(1, 2)
+		};
+		Vector2d[] hitboxVertices4 = {
+				new Vector2d(1, 1),
+				new Vector2d(2, 0),
+				new Vector2d(1, 2)
 		};
 		Hitbox hitbox = new Hitbox(hitboxVertices);
 		Hitbox hitbox2 = new Hitbox(hitboxVertices2);
-		Entity e1 = new Entity(new Vector2d(1, 1), hitbox);
-		Entity e2 = new Entity(new Vector2d(2, 1), hitbox2);
+		Hitbox hitbox3 = new Hitbox(hitboxVertices3);
+		Hitbox hitbox4 = new Hitbox(hitboxVertices4);
+		ArrayList<Hitbox> hitboxes = new ArrayList<Hitbox>();
+		ArrayList<Hitbox> hitboxes2 = new ArrayList<Hitbox>();
+		hitboxes.add(hitbox);
+		hitboxes.add(hitbox2);
+		hitboxes2.add(hitbox3);
+		hitboxes2.add(hitbox4);
+		Entity e = new Entity(new Vector2d(1, 0), hitboxes);
+		Entity e2 = new Entity(new Vector2d(2, 2.5), hitboxes2);
 		
-		System.out.println(e1.collides(e2));
+		System.out.println(e.collides(e2));
 	}
 
 }
